a0e9185eddfa66bd60380c11a461737b32f2bcf8
Dominic-wells <joedomwells@gmail.com>
2023-05-01 12:03:21 +0100

    add templates +allauth

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,6 +84,18 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+email_backend = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SINGUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
